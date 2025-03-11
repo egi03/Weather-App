3581551e060e1afdd754dd12085208abf817d796
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-03-11 20:39:36 +0100

    Add Poppins font and Typography

--- app/src/main/java/com/example/weatherapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Type.kt
@@ -3,8 +3,12 @@ package com.example.weatherapp.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.googlefonts.Font
+import com.example.weatherapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +35,30 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+// Define the Poppins Font Family
+val Poppins = FontFamily(
+    androidx.compose.ui.text.font.Font(R.font.poppins_medium, weight = FontWeight.Medium),
+    androidx.compose.ui.text.font.Font(R.font.poppins_bold, weight = FontWeight.Bold),
+    androidx.compose.ui.text.font.Font(R.font.poppins_light, weight = FontWeight.Light)
+)
+
+// Define typography using Poppins
+val PoppinsTypography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp
+    )
 )
