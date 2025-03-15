8ef37fd7535ff7c829679f40694b80966a3b54e5
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-03-15 14:37:52 +0100

    Add functions for parsing date and time

---
+++ app/src/main/java/com/example/weatherapp/utils/GeneralHelpers.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapp.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+object GeneralHelpers {
+    fun formatTimeFromUnix(unixTime: Number): String {
+        return try{
+            val timeInMillis = unixTime.toLong() * 1000L
+            val date = Date(timeInMillis)
+            val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+            dateFormat.format(date)
+        } catch (e: Exception){
+            "--:--"
+        }
+    }
+
+    fun formatDate(date: String): String {
+        return try{
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val dateObj = dateFormat.parse(date)
+            val newDateFormat = SimpleDateFormat("dd MMM", Locale.getDefault())
+            newDateFormat.format(dateObj!!)
+        } catch (e: Exception){
+            "--"
+        }
+    }
+}
