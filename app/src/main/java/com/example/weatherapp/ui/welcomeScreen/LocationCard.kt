6390e11a1cd7f5665796ab74382467397ed19f5f
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-03-15 22:56:28 +0100

    Modify for offline mode

--- app/src/main/java/com/example/weatherapp/ui/welcomeScreen/LocationCard.kt
+++ app/src/main/java/com/example/weatherapp/ui/welcomeScreen/LocationCard.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.ui.welcomeScreen
 
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,15 +20,18 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.weatherapp.Routes
+import com.example.weatherapp.navigation.Routes
 import com.example.weatherapp.ViewModels.WelcomeScreenViewModel
+import com.example.weatherapp.database.SharedPrefsManager
 import com.example.weatherapp.utils.GeneralHelpers.capitalizeWords
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
 fun LocationCard(text: String,
                  navController: NavController,
-                 viewModel: WelcomeScreenViewModel
+                 viewModel: WelcomeScreenViewModel,
+                 isOnline: Boolean = true
 ) {
     val coroutineScope = rememberCoroutineScope()
 
@@ -43,40 +47,10 @@ fun LocationCard(text: String,
             )
             .padding(16.dp)
             .clickable(onClick = {
-                coroutineScope.launch {
-                    try {
-                        val (weatherData, forecastData) = viewModel.getAllWeatherData(text)
-
-                        when {
-                            weatherData == null || forecastData == null -> {
-                                Toast.makeText(
-                                    context,
-                                    "Failed to fetch weather data",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            weatherData.cod == 200 && forecastData.cod == "200" -> {
-                                navController.currentBackStackEntry?.savedStateHandle?.apply {
-                                    set("weatherData", weatherData)
-                                    set("forecastData", forecastData)
-                                }
-                                navController.navigate(Routes.REPORT_SCREEN)
-                            }
-                            else -> {
-                                Toast.makeText(
-                                    context,
-                                    "City not found",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-                    } catch (e: Exception) {
-                        Toast.makeText(
-                            context,
-                            "Error: ${e.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                if (isOnline){
+                    handleOnlineClick(text, coroutineScope, viewModel, navController, context)
+                } else {
+                    handleOfflineClick(text, navController, context)
                 }
             })
 
@@ -90,4 +64,59 @@ fun LocationCard(text: String,
     }
     Spacer(modifier = Modifier.height(16.dp))
 
+}
+
+
+
+private fun handleOnlineClick(
+    text: String,
+    coroutineScope: CoroutineScope,
+    viewModel: WelcomeScreenViewModel,
+    navController: NavController,
+    context: Context
+) {
+    coroutineScope.launch {
+        try {
+            val (weatherData, forecastData) = viewModel.getAllWeatherData(text)
+            when {
+                weatherData == null || forecastData == null -> {
+                    Toast.makeText(context, "Failed to fetch weather data", Toast.LENGTH_SHORT).show()
+                }
+                weatherData.cod == 200 && forecastData.cod == "200" -> {
+                    navController.currentBackStackEntry?.savedStateHandle?.apply {
+                        set("weatherData", weatherData)
+                        set("forecastData", forecastData)
+                    }
+                    navController.navigate(Routes.REPORT_SCREEN)
+                }
+                else -> {
+                    Toast.makeText(context, "City not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(context, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+private fun handleOfflineClick(
+    text: String,
+    navController: NavController,
+    context: Context
+) {
+    val sharedPrefsManager = SharedPrefsManager(context)
+    val weatherData = sharedPrefsManager.getWeather(text)
+    val forecastData = sharedPrefsManager.getForecast(text)
+    val dateTime = sharedPrefsManager.getDateTime(text)
+
+    if (weatherData != null && forecastData != null) {
+        navController.currentBackStackEntry?.savedStateHandle?.apply {
+            set("weatherData", weatherData)
+            set("forecastData", forecastData)
+            set("dateTime", dateTime)
+        }
+        navController.navigate(Routes.OFFLINE_SCREEN)
+    } else {
+        Toast.makeText(context, "No cached data available", Toast.LENGTH_SHORT).show()
+    }
 }
