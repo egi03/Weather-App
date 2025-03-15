ae23bb845bd1cba6382dad692d73ea554bf9f2f1
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-03-15 17:14:20 +0100

    Add function to capitalize words

--- app/src/main/java/com/example/weatherapp/utils/GeneralHelpers.kt
+++ app/src/main/java/com/example/weatherapp/utils/GeneralHelpers.kt
@@ -26,4 +26,11 @@ object GeneralHelpers {
             "--"
         }
     }
+
+    fun String.capitalizeWords(delimiter: String = " "): String {
+        return split(delimiter).joinToString(delimiter) { word ->
+            val smallCaseWord = word.lowercase()
+            smallCaseWord.replaceFirstChar(Char::titlecaseChar)
+        }
+    }
 }
